d6c180bdfb79f181f4d398b66776d3601af68aa8
mtri9904 <minhtritamm@gmail.com>
2025-05-02 21:00:52 +0700

    Update Type Comment

--- QuanLyTruyenThong_TuVan/Models/Comment.cs
+++ QuanLyTruyenThong_TuVan/Models/Comment.cs
@@ -21,8 +21,8 @@ namespace QuanLyTruyenThong_TuVan.Models
         [StringLength(255)]
         public string ImageUrl { get; set; }
 
-        [StringLength(50)]
-        public string Type { get; set; }
+        [Required]
+        public CommentType Type { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.Now;
 
@@ -39,5 +39,16 @@ namespace QuanLyTruyenThong_TuVan.Models
         Completed,
         Failed
     }
-
+    public enum CommentType
+    {
+        Hygiene_And_Environment,
+        Security_And_Safety,
+        Technical_And_Maintenance,
+        Shared_Amenities,
+        ServiceFees_And_FinancialMatters,
+        Rules_And_Regulations,
+        StaffAttitude_And_ManagementService,
+        Communication_And_Information,
+        Other
+    }
 }
